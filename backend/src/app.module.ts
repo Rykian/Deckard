cfa5add9e0fc7409fc22c528310f18758f5982ff
Thibault Lacroux <thibault.lacroux@gmail.com>
2026-02-07 21:55:48 +0100

    chore: upgrade NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,9 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
-import { PluginDefinition } from 'apollo-server-core'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
-import apolloLogger from 'src/apollo-logger'
 import { resolve } from 'path'
 import { OBSModule } from './obs/_.module'
 import { WoWModule } from './wow/module'
-import { plugin as ApolloTracing } from 'apollo-tracing'
 import { MusicModule } from './music/music.module'
 import { ConfigModule } from '@nestjs/config'
 import { TwitchModule } from './twitch/module'
@@ -26,7 +23,6 @@ import { StreamModule } from './stream/module'
       driver: ApolloDriver,
       autoSchemaFile: resolve('..', 'schema.graphql'),
       installSubscriptionHandlers: true,
-      plugins: [ApolloTracing() as PluginDefinition, apolloLogger],
     }),
     OBSModule,
     MusicModule,
